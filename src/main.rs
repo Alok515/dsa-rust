e61bea4dec318104baabdca561268442f87f961c
Alok Kumar Singh <kumarsinghalok28@gmail.com>
2025-11-02 00:17:39 +0530

    feat: add sum function and print result

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+fn sum(x: i32, y: i32) -> i32 { 
+    x + y // sum(x, y)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let a = 10;
+    let mut b = 5;
+    b = 15;
+    let result = sum(a, b);
+    println!("The sum of {} and {} is {}", a, b, result);
 }
