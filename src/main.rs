f43ec9b7d5a9b7c79d7b1ef002ab059acc383c8e
Alok Kumar Singh <kumarsinghalok28@gmail.com>
2025-11-02 23:00:19 +0530

    feat: implement methods and associated functions for Rectangle

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,33 @@ struct Rectangle {
     height: u32
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+// fn area(rectangle: &Rectangle) -> u32 {
+//     rectangle.width * rectangle.height
+// }
+
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size
+        }
+    }
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
-fn print_area( area: &u32) {
+fn print_area( area: u32) {
     println!("The area of the rectangle is {} square pixels.", area);
+}
+
+fn can_hold_print( rect_a: &str, react_b: &str, hold: bool) {
+    println!("Can {} hold {} = {}", rect_a, react_b, hold);
 }
 
 fn main() {
@@ -17,6 +38,17 @@ fn main() {
         height: 50
     };
 
-    let rect1_area = area(&rect1);
-    print_area(&rect1_area);
+    let rect1_area = rect1.area();
+    print_area(rect1_area);
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40
+    };
+
+    let rect3 = Rectangle::square(25);
+
+    can_hold_print("rect1", "rect2", rect1.can_hold(&rect2));
+    can_hold_print("rect1", "rect3", rect1.can_hold(&rect3));
+    can_hold_print("rect2", "rect3", rect2.can_hold(&rect3));
 }
