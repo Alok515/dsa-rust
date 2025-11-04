a44edb4e22f4a7cfa3b873a45dc05ee92e34117d
Alok Kumar Singh <kumarsinghalok28@gmail.com>
2025-11-04 18:49:20 +0530

    feat: implement WebEvent enum and match

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,29 @@
-struct Rectangle {
-    width: u32,
-    height: u32
+enum WebEvent {
+    PageLoad,
+    PageUnload,
+    KeyPress(char),
+    Click { x: i32, y: i32 }
 }
 
-// fn area(rectangle: &Rectangle) -> u32 {
-//     rectangle.width * rectangle.height
-// }
-
-impl Rectangle {
-    fn square(size: u32) -> Rectangle {
-        Rectangle {
-            width: size,
-            height: size
-        }
+fn inspect(event: WebEvent) {
+    match event {
+        WebEvent::PageLoad => println!("page loaded"),
+        WebEvent::PageUnload => println!("page unloaded"),
+        WebEvent::KeyPress(c) => println!("pressed '{}'.", c),
+        WebEvent::Click { x, y } => println!("clicked at x={}, y={}.", x, y),
     }
-
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
-}
-
-fn print_area( area: u32) {
-    println!("The area of the rectangle is {} square pixels.", area);
-}
-
-fn can_hold_print( rect_a: &str, react_b: &str, hold: bool) {
-    println!("Can {} hold {} = {}", rect_a, react_b, hold);
 }
 
 fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50
-    };
+    let load = WebEvent::PageLoad;
+    let unload = WebEvent::PageUnload;
+    let press = WebEvent::KeyPress('x');
+    let click1 = WebEvent::Click { x: 100, y: 250 };
+    let click2 = WebEvent::Click { x: 0, y: 0 };
 
-    let rect1_area = rect1.area();
-    print_area(rect1_area);
-
-    let rect2 = Rectangle {
-        width: 10,
-        height: 40
-    };
-
-    let rect3 = Rectangle::square(25);
-
-    can_hold_print("rect1", "rect2", rect1.can_hold(&rect2));
-    can_hold_print("rect1", "rect3", rect1.can_hold(&rect3));
-    can_hold_print("rect2", "rect3", rect2.can_hold(&rect3));
+    inspect(load);
+    inspect(unload);
+    inspect(press);
+    inspect(click1);
+    inspect(click2);
 }
