d31928fb6d25e4eed4ddf22e3a0357655e3a1eeb
Alok Kumar Singh <kumarsinghalok28@gmail.com>
2025-11-07 02:51:53 +0530

    feat: implement error handling with Result

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
-use std::collections::HashMap;
 
+#[derive(Debug)]
+pub enum DivisonError {
+    DivideByZero
+}
 
-fn main () {
-    let text = String::from("hello world wonderful world");
-    let mut map: HashMap<String, u32> = HashMap::new();
+fn safe_divide(numerator: f64, denominator: f64) -> Result<f64, DivisonError> {
+    if denominator == 0.0 {
+        Err(DivisonError::DivideByZero)
+    } else {
+        Ok(numerator / denominator)
+    }
+}
 
-    for word in text.split_whitespace() {
-        let count = map.entry(word.to_string()).or_insert(0);
-        *count += 1;
+fn main() {
+    let val1 = safe_divide(10.0, 2.0);
+    let val2 = safe_divide(0.0, 0.0);
+
+    match val1 {
+        Ok(res) => println!("Result: {}", res),
+        Err(err) => println!("Error: {:?}", err)
     }
 
-    println!("{:?}", map);
+    match val2 {
+        Ok(res) => println!("Result: {}", res),
+        Err(err) => println!("Error: {:?}", err)
+    }
 }
