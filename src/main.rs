23ac2d248da6fbbc43116095976b0f53954037b7
Alok Kumar Singh <kumarsinghalok28@gmail.com>
2025-11-07 11:23:35 +0530

    feat: define skeleton for Linked List

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,22 @@
 
-#[derive(Debug)]
-pub enum DivisonError {
-    DivideByZero
-}
+mod list {
+    struct Node {
+        value: i32,
+        next: Option<Box<Node>>,
+    }
 
-fn safe_divide(numerator: f64, denominator: f64) -> Result<f64, DivisonError> {
-    if denominator == 0.0 {
-        Err(DivisonError::DivideByZero)
-    } else {
-        Ok(numerator / denominator)
+    pub struct List {
+        head: Option<Box<Node>>,
+    }
+
+    impl List {
+        pub fn new() -> List {
+            List { head: None }
+        }
     }
 }
 
 fn main() {
-    let val1 = safe_divide(10.0, 2.0);
-    let val2 = safe_divide(0.0, 0.0);
-
-    match val1 {
-        Ok(res) => println!("Result: {}", res),
-        Err(err) => println!("Error: {:?}", err)
-    }
-
-    match val2 {
-        Ok(res) => println!("Result: {}", res),
-        Err(err) => println!("Error: {:?}", err)
-    }
+    use list::List;
+    let my_list = List::new();
 }
